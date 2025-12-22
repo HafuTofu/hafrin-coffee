f83152ff0b0889a66f940dbbb6deb42de7853ecf
haflm248@gmail.com <hafizh.laththuf.muhammad@mhs.unj.ac.id>
2025-12-23 00:28:16 +0700

    Deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Lint errors won't block production builds (helpful while upgrading)
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
